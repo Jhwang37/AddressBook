4f664580422ffb8e23f33baa02442f2b0ebc4644
GBTC440009UR <GBTC440009UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-06 10:10:54 -0500

    first commit

---
+++ src/com/company/AddressBook.java
@@ -0,0 +1,65 @@
+package com.company;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+public class AddressBook {
+
+    private String email;
+    private String phoneNumber;
+
+    public AddressBook(String email, String phoneNumber){
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        isEmailValid(email);
+        isPhoneNumberValid(phoneNumber);
+        System.out.println("Email: " + getEmail());
+        System.out.println("Phone Number: " + getPhoneNumber());
+    }
+
+
+    private boolean isEmailValid(String email){
+        boolean isValid = false;
+
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        CharSequence inputStr = email;
+        //Make the comparison case-insensitive.
+        Pattern pattern = Pattern.compile(expression,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+        if(matcher.matches()){
+            isValid = true;
+        }
+        return isValid;
+    }
+
+    private boolean isPhoneNumberValid(String phoneNumber){
+        boolean isValid = false;
+
+        String expression = "^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$";
+        CharSequence inputStr = phoneNumber;
+        Pattern pattern = Pattern.compile(expression);
+        Matcher matcher = pattern.matcher(inputStr);
+        if(matcher.matches()){
+            isValid = true;
+        }
+        return isValid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AddressBook address = new AddressBook("Jhwang37", "301-325-8833");
+
+        }
+
+    }
+
